a2c9bf8d3a280e04749a046d08ae39843cdd7db2
vedashruta <vedashrutabhat@gmail.com>
2024-11-10 13:10:30 +0530

    [NEW] Additional Easy level problems

--- arrays/arrays.go
+++ arrays/arrays.go
@@ -2,6 +2,7 @@ package arrays
 
 import (
 	"math"
+	"strings"
 )
 
 func MaximumArraySum(nums []int) int {
@@ -31,13 +32,126 @@ you need to do the following things:
 
 func RemoveDuplicates(nums []int) int {
 	index := 0
-	for i := 1; i < len(nums); i++ {
-		if nums[i] != nums[i-1] {
+	for i := range nums {
+		if nums[index] != nums[i] {
 			index++
 			nums[index] = nums[i]
 		}
 	}
 	return index + 1
+}
+
+/*
+27. Remove Element
+Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed.
+Then return the number of elements in nums which are not equal to val.
+Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do
+the following things:
+
+1. Change the array nums such that the first k elements of nums contain the elements which are not equal to val.
+The remaining elements of nums are not important as well as the size of nums.
+2. Return k.
+
+Example:
+
+Input: nums = [0,1,2,2,3,0,4,2], val = 2
+Output: 5, nums = [0,1,4,0,3,_,_,_]
+*/
+func RemoveElement(nums []int, val int) int {
+	index := 0
+	for i := range nums {
+		if nums[i] != val {
+			nums[index] = nums[i]
+			index++
+		}
+	}
+	return index
+}
+
+/*
+28. Find the Index of the First Occurrence in a String
+Given two strings needle and haystack, return the index of the first occurrence of needle in
+haystack, or -1 if needle is not part of haystack.
+
+Example:
+
+Input: haystack = "sadbutsad", needle = "sad"
+Output: 0
+Explanation: "sad" occurs at index 0 and 6.
+The first occurrence is at index 0, so we return 0.
+*/
+
+// Solution 1
+func StrStr1(haystack string, needle string) int {
+	if len(needle) > len(haystack) {
+		return -1
+	}
+	for i := 0; i < len(haystack); i++ {
+		// for i := 0; i < len(haystack)+1-len(needle); i++ {
+		for j := 0; j < len(haystack); j++ {
+			if haystack[i+j] != needle[j] {
+				break
+			}
+			if j == len(needle)-1 {
+				return i
+			}
+		}
+	}
+	return -1
+}
+
+// Solution 2
+func StrStr2(haystack string, needle string) int {
+	if len(needle) > len(haystack) {
+		return -1
+	}
+	if strings.Contains(haystack, needle) {
+		return strings.Index(haystack, needle)
+	}
+	return -1
+}
+
+/*
+35. Search Insert Position
+Given a sorted array of distinct integers and a target value, return the index if the target is
+found. If not, return the index where it would be if it were inserted in order.
+You must write an algorithm with O(log n) runtime complexity.
+
+Example 1:
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+
+Example 2:
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+*/
+
+// Solution 1 O(n) time complexity
+func SearchInsert1(nums []int, target int) int {
+	i := 0
+	for i := range nums {
+		if nums[i] == target || target < nums[i] {
+			return i
+		}
+	}
+	return i
+}
+
+// Solution 2 O(log n) time complexity
+func SearchInsert2(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := (left + right) / 2
+		if nums[mid] == target {
+			return mid
+		}
+		if nums[mid] > target {
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+	return left
 }
 
 // Container With Most Water
--- arrays/inputs.go
+++ arrays/inputs.go
@@ -12,8 +12,28 @@ func Input() {
 	// target := 6
 	// fmt.Println(TwoSum(nums, target))
 
-	nums := []int{1, 2, 2, 3, 4}
-	k := RemoveDuplicates(nums)
-	fmt.Println(nums[:k])
+	// nums := []int{1, 2, 2, 3, 4}
+	// k := RemoveDuplicates(nums)
+	// fmt.Println(nums[:k])
+
+	// nums := []int{0, 1, 2, 2, 3, 0, 4, 2}
+	// val := 2
+	// k := RemoveElement(nums, val)
+	// fmt.Println(nums[:k])
+	// haystack := "sadbutsad"
+	// needle := "sad"
+	// k := StrStr1(haystack, needle)
+	// fmt.Println(haystack[k:len(needle)])
+	// k = StrStr2(haystack, needle)
+	// fmt.Println(haystack[k:len(needle)])
+	nums := []int{1, 3, 5, 6}
+	target := 5
+	k := SearchInsert1(nums, target)
+	fmt.Println(k)
+	target = 2
+	k = SearchInsert1(nums, target)
+	fmt.Println(k)
+	k = SearchInsert2(nums, target)
+	fmt.Println(k)
 
 }
